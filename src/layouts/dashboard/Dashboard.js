c3588ecd351484f022529baf7c04771be5ce9b2b
oca <leosuzin1126@gmail.com>
2018-08-20 14:27:56 +1000

    user list

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -1,9 +1,87 @@
 import React, { Component } from 'react'
+import { Table, Button } from 'semantic-ui-react'
 
 class Dashboard extends Component {
   constructor(props, { authData }) {
     super(props)
     authData = this.props
+
+    this.state = {};
+    this.state.datas = [{
+      name: 'aaa',
+      email: 'email1.e.com',
+      address: 'address1',
+      userType: 'Buyer',
+      userState: 'Pending'
+    }, {
+      name: '21433214',
+      email: 'email2.e.com',
+      address: 'address1',
+      userType: 'Seller',
+      userState: 'Pending'
+    }, {
+      name: '334354654',
+      email: 'email3.e.com',
+      address: 'address1',
+      userType: 'Owner',
+      userState: 'Approved'
+    }]
+  }
+
+  //{Pending, Approved}
+  showState(state) {
+    if (state == 'Pending')
+      return (<Table.Cell negative>{state}</Table.Cell>);
+    else
+      return (<Table.Cell positive>{state}</Table.Cell>);
+  }
+
+  showTypeBtn(curType, tgtType, title, email) {
+    if (curType == tgtType)
+      return (<Button>{tgtType}</Button>);
+    else
+      return (<Button positive onClick={() => this.onClickUserType(tgtType, email)}>{tgtType}</Button>);
+  }
+  showTypeBtns(type, email) {
+    return (
+      <Table.Cell>
+        <Button.Group>
+          {this.showTypeBtn(type, 'Buyer', 'Buyer', email)}
+          <Button.Or />
+          {this.showTypeBtn(type, 'Seller', 'Seller', email)}
+          <Button.Or />
+          {this.showTypeBtn(type, 'Owner', 'Owner', email)}
+        </Button.Group>
+      </Table.Cell>);
+  }
+
+  showStateBtns(state, email) {
+    if (state == 'Approved') {
+      return (
+        <Table.Cell>
+          <Button.Group>
+            <Button onClick={() => this.onClickUserState('Pending', email)}>Pending</Button>
+            <Button.Or />
+            <Button positive>Approve</Button>
+          </Button.Group>
+        </Table.Cell>);
+    } else {
+      return (
+        <Table.Cell>
+          <Button.Group>
+            <Button negative>Pending</Button>
+            <Button.Or />
+            <Button onClick={() => this.onClickUserState('Approved', email)}>Approve</Button>
+          </Button.Group>
+        </Table.Cell>);
+    }
+  }
+
+  onClickUserType(type, email) {
+    alert(type + "   " + email);
+  }
+  onClickUserState(state, email) {
+    alert(state + "   " + email);
   }
 
   render() {
@@ -13,6 +91,44 @@ class Dashboard extends Component {
           <div className="pure-u-1-1">
             <h1>Dashboard</h1>
             <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Table celled structured>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell rowSpan='2'>No</Table.HeaderCell>
+                  <Table.HeaderCell rowSpan='2'>Name</Table.HeaderCell>
+                  <Table.HeaderCell rowSpan='2'>email</Table.HeaderCell>
+                  <Table.HeaderCell rowSpan='2'>address</Table.HeaderCell>
+                  <Table.HeaderCell rowSpan='2'>User type</Table.HeaderCell>
+                  <Table.HeaderCell rowSpan='2'>User state</Table.HeaderCell>
+                  <Table.HeaderCell colSpan='2'>Action</Table.HeaderCell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.HeaderCell>Change user type</Table.HeaderCell>
+                  <Table.HeaderCell>Change user state</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+              {this.state.datas.map((item, index) => {
+                return ( <Table.Row key={item.email}>
+                          <Table.Cell>{index + 1}</Table.Cell>
+                          <Table.Cell>{item.name}</Table.Cell>
+                          <Table.Cell>{item.email}</Table.Cell>
+                          <Table.Cell>{item.address}</Table.Cell>
+                          <Table.Cell>{item.userType}</Table.Cell>
+                          {this.showState(item.userState)}
+
+                          {this.showTypeBtns(item.userType, item.email)}
+                          {this.showStateBtns(item.userState, item.email)}
+                        </Table.Row> )})
+              }
+                
+              </Table.Body>
+            </Table>
           </div>
         </div>
       </main>
